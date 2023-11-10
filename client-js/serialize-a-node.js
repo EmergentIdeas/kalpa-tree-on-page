64a04efb3afa745cff4d816fe023f7a2796d6ff3
Dan Kolz <dan@emergentideas.com>
2023-11-10 09:09:50 -0600

    fix serialize bug

--- client-js/serialize-a-node.js
+++ client-js/serialize-a-node.js
@@ -2,7 +2,7 @@ function serializeANode (tree, rootId, result) {
 	tree.children(rootId).forEach(function (child) {
 		child.parentId = rootId
 		result.push(child)
-		serialize(tree, child.id, result)
+		serializeANode(tree, child.id, result)
 	})
 }
 
